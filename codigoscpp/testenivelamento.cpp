61c063a11bd4755383a4a250c2b4b33efa8c0bad
derekmnoble <84161499+derekmnoble@users.noreply.github.com>
2021-06-27 22:39:13 -0300

    iniciei um teste de nivelamento

---
+++ codigoscpp/testenivelamento.cpp
@@ -0,0 +1,19 @@
+ #include <iostream>
+
+using namespace std;
+
+int main() {
+
+  char resposta;
+  string username;
+
+  std::cout << "What is your name?\n";
+  std::cin >> username;
+
+  std::cout << "Neste teste de nivelamento em inglês,\n";
+  std::cout << "Você vai responder perguntas de múltipla escolha.\n";
+  std::cout << "Boa sorte, " << username;
+
+  return 0;
+}
+
